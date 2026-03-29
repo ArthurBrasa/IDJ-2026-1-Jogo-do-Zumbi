b8f0213206180b5f319ba0b059dac79b76cff925
Arthur Brasa <arthurbrasa@outlook.com>
2026-03-29 10:57:30 -0300

    Sprite.h

--- include/Sprite.h
+++ include/Sprite.h
@@ -1,7 +1,25 @@
 #ifndef SPRITE_H
 #define SPRITE_H
+#include <iostream>
 
-class Sprite {};
+class Sprite {
+    public:
+        Sprite();
+        Sprite(std::string file);
+        ~Sprite();
+
+        void open(std::string file);
+        void setClip(int x, int y, int w, int h);
+        void render(int x, int y);
+        int getWidth();
+        int getHeight();
+        bool isOpen();
+    private:
+        SDL_Texture* texture;
+        SDL_Rect clipRect;
+        int width;
+        int height;
+};
 
 
 #endif
